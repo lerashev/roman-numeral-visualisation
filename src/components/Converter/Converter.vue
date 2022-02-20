b416613cd844c142d1894f2c2299fac74ca518aa
Nick <kuznaik@gmail.com>
2022-02-20 17:57:18 -0600

    components skeleton

---
+++ src/components/Converter/Converter.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="converter">
+        <input class="input arabic" />
+        <button class="button-convert">Convert</button>
+        <input class="input roman" />
+        <!-- v-for line in romanBreakdown <div> letter : meaning -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Converter",
+    props: {},
+};
+</script>
+
+<style scoped>
+#converter {
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+}
+</style>
